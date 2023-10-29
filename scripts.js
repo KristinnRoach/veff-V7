9f6aee43ae24331e7c6993a6aef47d3a38bd7859
KristinnRoach <kristinnroach@gmail.com>
2023-10-29 23:51:11 +0000

    rann út á tíma

--- scripts.js
+++ scripts.js
@@ -1,29 +1,4 @@
 /**
- * Verkefnalýsing fyrir verkefni 7 með mörgum athugasemdum, sjá einnig yfirferð í fyrirlestri 9.
- * Sjá `scripts-plain.js` fyrir lausn án athugasemda.
- * Föll og breytur eru skjalaðar með jsdoc, athugasemdir á þessu:
- * // formi eru til nánari útskýringa.
- * eru aukalega og ekki nauðsynlegar.
- * Kóðabútar eru innan ``.
- *
- * @see https://jsdoc.app/
- */
-
-// Til að byrja með skilgreinum við gögn sem við notum í forritinu okkar. Við þurfum að skilgreina
-// - Vörur sem er hægt að kaupa
-// - Körfu sem geymir vörur sem notandi vill kaupa
-// Í báðum tilfellum notum við gagnaskipan (e. data structure) með því að nota hluti (objects),
-// fylki (array) og grunn gildi (e. primitive values) eins og tölur (numbers) og strengi (string).
-
-// Hér notum við _typedef_ til að skilgreina hvernig Product hluturinn okkar lítur út.
-// Þetta er ekki JavaScript heldur sérstök skilgreining frá JSDoc sem VSCode notar til að hjálpa
-// okkur við að skrifa með því að birta intellisense/autocomplete og hugsanlega sýna villur.
-// Við getum látið VSCode sýna okkur villur:
-// - Opna „Settings“ með Cmd + , (macOS) eða Ctrl + , (Windows) og slá „check js“ í leitargluggann.
-// - Velja „JavaScript › Implicit Project Config: Check JS“ og haka í.
-// https://code.visualstudio.com/docs/nodejs/working-with-javascript#_type-checking-javascript
-
-/**
  * @typedef {Object} Product
  * @property {number} id Auðkenni vöru, jákvæð heiltala stærri en 0.
  * @property {string} title Titill vöru, ekki tómur strengur.
@@ -31,37 +6,16 @@
  * @property {number} price Verð á vöru, jákvæð heiltala stærri en 0.
  */
 
-// Við viljum geta haft fleiri en eina vöru þannig að við þurfum að hafa fylki af vörum.
-// Við byrjum með fylki sem hefur færslur en gætum síðan í forritinu okkar bætt við vörum.
-
 /**
  * Fylki af vörum sem hægt er að kaupa.
  * @type {Array<Product>}
  */
 const products = [
-  // Fyrsta stak í fylkinu, verður aðgengilegt sem `products[0]`
   {
-    // Auðkennið er eitthvað sem við bara búum til sjálf, verður að vera einkvæmt en engin regla í
-    // JavaScript passar upp á það.
-    // Þar sem það er aðeins ein tegund af tölum í JavaScript þá verðum við að passa okkur hér að
-    // nota heiltölu, ekkert sem bannar okkur að setja `1.1`.
-    // Ef við kveikjum á að VSCode sýni villur og við breytum þessu í t.d. streng munum við sjá
-    // villu með rauðum undirlínum og færslu í `Problems` glugganum.
     id: 1,
-
-    // Titill er strengur, en gæti verið „tómi strengurinn“ (e. empty string) sem er bara `''`.
-    // JavaScript gerir ekki greinarmun á tómum streng og strengjum sem innihalda eitthvað.
-    // Við gætum líka notað `""` eða ` `` ` (backticks) til að skilgreina strengi en venjan er að
-    // nota einfaldar gæsalappir/úrfellingarkommur (e. single quotes).
     title: 'HTML húfa',
-
-    // Hér skilgreinum við streng í nýrri línu á eftir skilgreiningu á lykli (key) í hlutnum.
     description:
       'Húfa sem heldur hausnum heitum og hvíslar hugsanlega að þér hvaða element væri best að nota.',
-
-    // Verð sem jákvæð heiltala. Getum líka notað `1000` en það er hægt að nota undirstrik (_) til
-    // að gera stórar tölur læsilegri, t.d. `100_000_000`.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#numeric_separators
     price: 5_000,
   },
   {
@@ -76,10 +30,7 @@ const products = [
     description: 'Mjög töff jakki fyrir öll sem skrifa JavaScript reglulega.',
     price: 20_000,
   },
-  // Hér gætum við bætt við fleiri vörum í byrjun.
 ];
-
-// Skilgreinum hluti sem halda utan um það sem notandi ætlar að kaupa.
 
 /**
  * @typedef {Object} CartLine
@@ -94,8 +45,6 @@ const products = [
  * @property {string|null} address Heimilisfang kaupanda ef skilgreint, annars `null`.
  */
 
-// Við notum `null` sem gildi fyrir `name` og `address` áður en notandi hefur skilgreint þau.
-
 /**
  * Karfa sem geymir vörur sem notandi vill kaupa.
  * @type {Cart}
@@ -105,11 +54,6 @@ const cart = {
   name: null,
   address: null,
 };
-
-// Nú höfum við skilgreint gögnin sem forritið okkar notar. Næst skilgreinum við föll sem vinna með
-// gögnin og inntak frá notanda.
-// Athugið að hér erum við að setja öll föll í sömu skrá og sama scope, það myndi hjálpa okkur að
-// setja föll í mismunandi skrár og nota módúla til að tengja saman, við gerum það í verkefni 8.
 
 // --------------------------------------------------------
 // Hjálparföll
@@ -121,7 +65,7 @@ const cart = {
  * const price = formatPrice(123000);
  * console.log(price); // Skrifar út `123.000 kr.`
  * @param {number} price Verð til að sníða.
- * @returns {string} Verð sniðið með íslenskum krónu.
+ * @returns Verð sniðið með íslenskum krónu.
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl
  */
 function formatPrice(price) {
@@ -130,10 +74,8 @@ function formatPrice(price) {
     currency: 'ISK',
     minimumFractionDigits: 0
   });
-  formatter.format(price);
-  return price.toString();
+  return formatter.format(price);
 }
-
 
 /**
  * Athuga hvort `num` sé heiltala á bilinu `[min, max]`.
@@ -143,7 +85,7 @@ function formatPrice(price) {
  * @returns `true` ef `num` er heiltala á bilinu `[min, max]`, annars `false`.
  */
 function validateInteger(num, min = 0, max = Infinity) {
-  return num <= num && num <= max;
+  return num >= min && num <= max;
 }
 
 /**
@@ -163,7 +105,7 @@ function formatProduct(product, quantity = undefined) {
     return `${product.title} - ${quantity}x${formatPrice(product.price)} samtals ${total}`;
 
   }
-  return `${product.title} - ${product.price}`;
+  return `${product.title} - ${formatPrice(product.price)}`;
 }
 
 /**
@@ -178,7 +120,13 @@ function formatProduct(product, quantity = undefined) {
  * @returns Streng sem inniheldur upplýsingar um körfu.
  */
 function cartInfo(cart) {
-  /* Útfæra */
+  if (cart.lines.length === 0) {
+    return 'Karfan er tóm.';
+  }
+
+  return cart.lines.map(line => 
+    `${formatProduct(line.product, line.quantity)}`
+  ).join('\n');
 }
 
 // --------------------------------------------------------
@@ -194,16 +142,6 @@ function cartInfo(cart) {
  * @returns undefined
  */
 function addProduct() {
-  // Til einföldunar gerum við ekki greinarmun á „Cancel“ og „Escape“ og tómum gildum frá notanda.
-
-  // Förum í gegnum hvort og eitt gildi sem við viljum og pössum að við höfum eitthvað gildi.
-  // Gildi sem við fáum í gegnum `prompt` eru annaðhvort `null` ef notandi ýtir á „Cancel“ eða „Esc“
-  // eða strengur.
-  // Ef við fáum ógilt gildi hættum við í fallinu með því að nota `return` sem skilar `undefined`.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return
-  // Þetta er kallað „early exit“ og er gott til að koma í veg fyrir að þurfa að skrifa auka
-  // skilyrði í if-setningum en getur valdið vandræðum í einhverjum tilfellum.
-  // https://en.wikipedia.org/wiki/Structured_programming#Early_exit
   const title = prompt('Titill:');
   if (!title) {
     console.error('Titill má ekki vera tómur.');
@@ -216,32 +154,20 @@ function addProduct() {
     return;
   }
 
-  // Gerum greinarmun á verði sem streng...
   const priceAsString = prompt('Verð:');
   if (!priceAsString) {
     console.error('Verð má ekki vera tómt.');
     return;
   }
 
-  // og síðan verði sem við getum unnið með sem tölu.
-  // Hér notum við `Number.parseInt` til að breyta streng í heiltölu.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/parseInt
   const price = Number.parseInt(priceAsString, 10);
 
-  // Athugum hvort við fáum löglega heiltölu sem er stærri en 0 með því að nota hjálparfallið okkar.
   if (!validateInteger(price, 1)) {
     console.error('Verð verður að vera jákvæð heiltala.');
     return;
   }
 
-  // Búum til auðkenni fyrir vöruna okkar sem einfaldlega næstu heiltölu út frá fjölda vara sem við
-  // höfum nú þegar.
   const id = products.length + 1;
-
-  // Búum til vöruna okkar sem hlut með því að nota „object literal“.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
-
-  // Notum jsdoc til að fá villuathugun í vscode
 
   /** @type {Product} */
   const product = {
@@ -251,14 +177,9 @@ function addProduct() {
     price,
   };
 
-  // Bætum vörunni aftast við fylkið okkar.
   products.push(product);
 
-  // Birtum upplýsingar um vöru sem við bjuggum til.
   console.info(`Vöru bætt við:\n${formatProduct(product)}`);
-
-  // Hér er ekkert return, þá skilar fallið `undefined`.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return
 }
 
 /**
@@ -270,8 +191,9 @@ function addProduct() {
  * @returns undefined
  */
 function showProducts() {
-  /* Útfæra */
-  /* Hér ætti að nota `formatPrice` hjálparfall */
+  products.forEach(product => {
+    console.log(`#${product.id} ${product.title} — ${product.description} — ${formatPrice(product.price)}`);
+  });
 }
 
 /**
@@ -290,11 +212,32 @@ function showProducts() {
  * @returns undefined
  */
 function addProductToCart() {
-  /* Útfæra */
+  const id = prompt('Auðkenni vöru:');
+  if (id === null) return;
 
-  /* Hér ætti að nota `validateInteger` hjálparfall til að staðfesta gögn frá notanda */
-  
-  /* Til að athuga hvort vara sé til í `cart` þarf að nota `cart.lines.find` */
+  if (!validateInteger(Number(id), 1)) {
+    console.error('Auðkenni vöru er ekki löglegt, verður að vera heiltala stærri en 0.');
+    return;
+  }
+
+  const product = products.find(p => p.id === Number(id));
+  if (!product) {
+    console.error('Vara fannst ekki.');
+    return;
+  }
+
+  const quantity = prompt('Fjöldi:');
+  if (!validateInteger(Number(quantity), 1, 99)) {
+    console.error('Fjöldi er ekki löglegur, lágmark 1 og hámark 99.');
+    return;
+  }
+
+  const cartLine = cart.lines.find(line => line.product.id === product.id);
+  if (cartLine) {
+    cartLine.quantity += Number(quantity);
+  } else {
+    cart.lines.push({ product, quantity: Number(quantity) });
+  }
 }
 
 /**
@@ -310,7 +253,13 @@ function addProductToCart() {
  * @returns undefined
  */
 function showCart() {
-  /* Útfæra */
+  if (cart.lines.length === 0) {
+    console.log('Karfan er tóm.');
+  } else {
+    cart.lines.forEach(line => {
+      console.log(formatProduct(line.product, line.quantity));
+    });
+  }
 }
 
 /**
@@ -332,5 +281,19 @@ function showCart() {
  * @returns undefined
  */
 function checkout() {
-  /* Útfæra */
+  if (cart.lines.length === 0) {
+    console.log('Karfan er tóm.');
+    return;
+  }
+
+  const name = prompt('Nafn:');
+  const address = prompt('Heimilisfang:');
+
+  if (!name || !address) {
+    console.error('Það vantar nafn eða heimilisfang.');
+    return;
+  }
+
+  console.log(`Pöntun móttekin ${name}.\nVörur verða sendar á ${address}.\n`);
+  showCart();
 }
